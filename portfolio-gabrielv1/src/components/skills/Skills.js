c22237c5a029949fdddb16e02cf9c43fcab306c1
Gabriel Ortiz <igabriel661@hotmail.com>
2023-03-15 11:10:17 -0700

    Began to upload logos of the skills I want to showcase.

--- portfolio-gabrielv1/src/components/skills/Skills.js
+++ portfolio-gabrielv1/src/components/skills/Skills.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
+import img from '../../images/java-logo.png'
 
 const Skills = () => {
   return (
     <div>
 
       <h1>Skills</h1>
+
+      <Image src={img} 
+             alt='' 
+             roundedCircle
+             width={100}
+             height={100}
+             >
+
+             </Image>
 
       
     </div>
