42b9c154f47e7dcc20136d15b06b137882143d50
Gabriel Ortiz <igabriel661@hotmail.com>
2023-03-16 23:42:44 -0700

    Find new images for the logos.

--- portfolio-gabrielv1/src/components/skills/Skills.js
+++ portfolio-gabrielv1/src/components/skills/Skills.js
@@ -7,15 +7,15 @@ const Skills = () => {
 
       <h1>Skills</h1>
 
-      <Image src={require('../../images/java-logo.png')} alt='' roundedCircle width={100} height={100} />
-      <Image src={require('../../images/react-logo.png')} alt='' roundedCircle width={100} height={100} />
-      <Image src={require('../../images/springboot-logo.png')} alt='' roundedCircle width={100} height={100} />
-      <Image src={require('../../images/aws-logo.png')} alt='' roundedCircle width={100} height={100} />
-      <Image src={require('../../images/mysql-logo.png')} alt='' roundedCircle width={100} height={100} />
-      <Image src={require('../../images/html-logo.png')} alt='' roundedCircle width={100} height={100} />
-      <Image src={require('../../images/css-logo.png')} alt='' roundedCircle width={100} height={100} />
-      <Image src={require('../../images/js-logo.png')} alt='' roundedCircle width={100} height={100} />
-      <Image src={require('../../images/git-logo.png')} alt='' roundedCircle width={100} height={100} />
+      <Image src={require('../../images/java-logo.png')} alt='' width={100} height={100} />
+      <Image src={require('../../images/react-logo.png')} alt='' width={100} height={100} />
+      <Image src={require('../../images/springboot-logo.png')} alt='' width={100} height={100} />
+      <Image src={require('../../images/aws-logo.png')} alt='' width={100} height={100} />
+      <Image src={require('../../images/mysql-logo.png')} alt='' width={100} height={100} />
+      <Image src={require('../../images/html-logo.png')} alt='' width={100} height={100} />
+      <Image src={require('../../images/css-logo.png')} alt='' width={100} height={100} />
+      <Image src={require('../../images/js-logo.png')} alt='' width={100} height={100} />
+      <Image src={require('../../images/git-logo.png')} alt='' width={100} height={100} />
       
     </div>
   )
