c6cba27b2476471754b3f74f9819cfc533d4ecdc
Gabriel Ortiz <igabriel661@hotmail.com>
2023-03-23 11:33:02 -0700

    Added some styling.

--- portfolio-gabrielv1/src/components/contact/Contact.js
+++ portfolio-gabrielv1/src/components/contact/Contact.js
@@ -1,37 +1,40 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>
+    <div className='contact-container'>
 
-    <h1>Contact</h1>
+      <h1>Contact</h1>
 
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Full Name</Form.Label>
-        <Form.Control type="name" placeholder="Full name" />
-      </Form.Group>
+      <Form className='contact-form'>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="name" placeholder="Full name" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="message" placeholder="Message" />
-      </Form.Group>
-      <div className="d-grid gap-2">
-        <Button variant="dark" type="submit" size='lg'>
-          Submit
-        </Button>
-      </div>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="message" placeholder="Message" />
+        </Form.Group>
+
+        <div className="d-grid gap-2">
+          <Button className='contact-btn' variant="dark" type="submit" size='lg'>
+            Submit
+          </Button>      
+        </div>
+
+      </Form>
 
     </div>
   )
--- portfolio-gabrielv1/src/components/skills/Skills.js
+++ portfolio-gabrielv1/src/components/skills/Skills.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import Image from 'react-bootstrap/Image'
+import './Skills.css'
 
 const Skills = () => {
   return (
-    <div>
+    <div className='skills-container'> 
 
       <h1>Skills</h1>
 
-      <Image src={require('../../images/java-logo.png')} alt='' width={100} height={100} />
-      <Image src={require('../../images/react-logo.png')} alt='' width={100} height={100} />
-      <Image src={require('../../images/springboot-logo.png')} alt='' width={100} height={100} />
-      <Image src={require('../../images/aws-logo.png')} alt='' width={100} height={100} />
-      <Image src={require('../../images/mysql-logo.png')} alt='' width={100} height={100} />
-      <Image src={require('../../images/html-logo.png')} alt='' width={100} height={100} />
-      <Image src={require('../../images/css-logo.png')} alt='' width={100} height={100} />
-      <Image src={require('../../images/js-logo.png')} alt='' width={100} height={100} />
-      <Image src={require('../../images/git-logo.png')} alt='' width={100} height={100} />
+      <div className='skills-images'>
+        <Image className='logos' src={require('../../images/java-logo.png')} alt='' width={100} height={100} />
+        <Image className='logos' src={require('../../images/react-logo.png')} alt='' width={100} height={100} />
+        <Image className='logos' src={require('../../images/springboot-logo.png')} alt='' width={100} height={100} />
+        <Image className='logos' src={require('../../images/aws-logo.png')} alt='' width={100} height={100} />
+        <Image className='logos' src={require('../../images/mysql-logo.png')} alt='' width={100} height={100} />
+        <Image className='logos' src={require('../../images/html-logo.png')} alt='' width={100} height={100} />
+        <Image className='logos' src={require('../../images/css-logo.png')} alt='' width={100} height={100} />
+        <Image className='logos' src={require('../../images/js-logo.png')} alt='' width={80} height={80} />
+        <Image className='logos' src={require('../../images/git-logo.png')} alt='' width={85} height={85} />
+      </div>
+      
       
     </div>
   )
