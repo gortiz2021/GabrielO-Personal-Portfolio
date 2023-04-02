1fe06e1c6dae8b8d4ee467560d85b8af156eebb6
Gabriel Ortiz <igabriel661@hotmail.com>
2023-04-02 09:14:18 -0700

    Added some styling. Utilized EmailJS to add functionality to the Contact component.

--- portfolio-gabrielv1/src/components/contact/Contact.js
+++ portfolio-gabrielv1/src/components/contact/Contact.js
@@ -1,31 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import emailjs from 'emailjs-com';
 import './Contact.css'
 
 const Contact = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_k4flyeu', 
+        'template_hvst8ml',
+        e.target,
+        'v9HggiIQF-0WWEG3o'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          alert('Message sent successfully!')
+        },
+        (error) => {
+          console.log(error.text)
+          alert("An error occurred, please try again later.")
+        }
+      );
+
+    setName('');
+    setEmail('');
+    setMessage('');
+
+  };
+
   return (
     <div className='contact-container'>
 
       <h1>Contact</h1>
 
-      <Form className='contact-form'>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form className='contact-form' onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Full Name</Form.Label>
-          <Form.Control type="name" placeholder="Full name" />
+          <Form.Control 
+            type="name" 
+            placeholder="Full name" 
+            name='name' 
+            value={name} 
+            onChange={ (e) => setName(e.target.value) }
+            required
+            />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Email" />
+          <Form.Control 
+            type="email" 
+            placeholder="Email" 
+            name='email'
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
+            required
+            />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="message" placeholder="Message" />
+        <Form.Group className="mb-3" controlId="formBasicMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control 
+            as='textarea'
+            rows={5}
+            placeholder='Enter message'
+            name='message' 
+            value={message}
+            onChange={ (e) => setMessage(e.target.value) }
+            required
+            />
         </Form.Group>
 
         <div className="d-grid gap-2">
--- portfolio-gabrielv1/src/components/projects/Projects.js
+++ portfolio-gabrielv1/src/components/projects/Projects.js
@@ -10,27 +10,27 @@ const Projects = () => {
       <div className='project-block'>
 
         <div className='project-item project-item-1'>
-          <h3>Movie App Review</h3>
+          <h3>Movie Review App</h3>
           
           <div className='project-detail project-detail-1'>
-            <Image className='project-image' src={require('../../images/java-logo.png')} alt='' width={100} height={100} />
+            <Image className='project-image' src={require('../../images/MovieReviewApp-img.png')} alt='' />
 
             <p className='project-text'> A website used to watch movie trailers. Users can leave a review about the movie and see previous reviews. Built with HTML/CSS/JS, React, MongoDB and Springboot.</p>
           </div>
           
         </div>
         
-        <div className='project-item project-item-1'>
+        <div className='project-item project-item-2'>
           <h3>Dollars Bank App</h3>
 
           <div className='project-detail project-detail-2'>
-            <Image className='project-image' src={require('../../images/java-logo.png')} alt='' width={100} height={100} />
+            <Image className='project-image' src={require('../../images/DollarsBank-Fullstack-img.png')} alt='' />
 
             <p className='project-text'> A banking application that uses REST APIs. The APIs wre created using Springboot and tested with Postman. Data was stored in a MySQL database. The UI was built with React, Bootstrap, and HTMl/CSS/JS. </p>
           </div>
         </div>
 
-        <div className='project-item project-item-1'>
+        {/* <div className='project-item project-item-3'>
           <h3 className='project3-header'>Virtual Reality Game Room</h3>
 
           <div className='project-detail project-detail-3'>
@@ -38,7 +38,7 @@ const Projects = () => {
 
             <p className='project-text'> An VR website that has various rooms the user can interact with. Built with MERN stack.</p>
           </div>
-        </div>
+        </div> */}
 
       </div>
 
